77ceefb0a44240a36dedef40304b3b1f7ab5d75a
ch1ppie <silly.ash@proton.me>
2025-07-24 13:46:15 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
 from flask import Flask
-
+from flask import request
+from flask import jsonify
+from time import time
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/license/id/core/<id>')
+def id():
+    return jsonify({
+        "owner": "ch1ppie",
+        "product": "DonutCore",
+        "ips": [request.remote_addr],
+        "creation_date": time.time()
+    }), 200
